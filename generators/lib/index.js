dc6286c22a66ad018e6e60bd88a8fbc13910d214
Michel Felipe <mfelipeof@gmail.com>
2019-06-06 00:25:34 -0300

    [WIP]: Normalize Android classes package folder + Initial AndroidManifest update support

--- generators/lib/index.js
+++ generators/lib/index.js
@@ -1,7 +1,10 @@
 const Generator = require('yeoman-generator');
-const xml2json = require('xml2json');
-const android = require('android-versions');
 const Octokit = require('@octokit/rest');
+const xml2json = require('xml2json');
+const xmlBeautifier = require('xml-beautifier');
+const android = require('android-versions');
+const fs = require('fs-extra');
+const path = require('path');
 
 module.exports = class extends Generator {
 
@@ -25,19 +28,27 @@ module.exports = class extends Generator {
             default: this.defaultRemote
         });
 
+        this.option('git-remote-token', {
+            alias: 'token',
+            type: String,
+            description: 'Github API token. Need this to use together with --git-fork option'
+        });
+
         // Github API with personal access token
         if (this.options.gitFork) {
 
-            if (this.fs.exists(this.destinationPath('.gitremote-token'))) {
+            this.remoteToken = process.env.GITREMOTE_TOKEN || this.options.gitRemoteToken;
 
-                this.remoteToken = this.fs.read(this.destinationPath('.gitremote-token'));
-                if (this.remoteToken && this.remoteToken.length > 0) {
+            // if (this.fs.exists(this.destinationPath('.gitremote-token'))) {
 
-                    this.remoteRest = new Octokit({
-                        auth: `token ${this.remoteToken}`
-                    });
-                }
-            }
+            //     this.remoteToken = this.fs.read(this.destinationPath('.gitremote-token'));
+        }
+
+        if (this.remoteToken && this.remoteToken.length > 0) {
+
+            this.remoteRest = new Octokit({
+                auth: `token ${this.remoteToken}`
+            });
         }
 
         // Is a Cordova Plugin ?
@@ -64,14 +75,14 @@ module.exports = class extends Generator {
                 type: 'input',
                 name: 'gitUsername',
                 message: 'Git remote username',
-                when: this.options.gitFork && !this.remoteToken,
+                when: !!(this.options.gitFork && !this.remoteToken),
                 validate: (value) => !value || value.length == 0 ? 'Username is required!' : true
             },
             {
                 type: 'password',
                 name: 'gitPassword',
                 message: 'Git remote password',
-                when: this.options.gitFork && !this.remoteToken,
+                when: !!(this.options.gitFork && !this.remoteToken),
                 validate: (value, answers) => {
 
                     if (!value || value.length == 0) {
@@ -94,14 +105,8 @@ module.exports = class extends Generator {
                 type: 'list',
                 name: 'remoteGroup',
                 message: 'Which remote organization/group to fork this project?',
-                when: this.options.gitFork && this.remoteRest,
+                when: !!(this.options.gitFork && this.remoteRest),
                 choices: async () => {
-
-                    // return await this.octokit.oauthAuthorizations.listAuthorizations().then((result) => {
-                    //     return result.data.map((data) => {
-                    //         return { name: data.token, value: data.token };
-                    //     });
-                    // });
 
                     return await this.remoteRest.orgs.listForAuthenticatedUser().then((result) => {
                         return result.data.map((data) => {
@@ -146,7 +151,7 @@ module.exports = class extends Generator {
                     return result.length - 1;
                 },
                 choices: () => {
-                    return this.getAndroidVersions()
+                    return this.getAndroidVersions();
                 }
             }
         ]);
@@ -156,7 +161,10 @@ module.exports = class extends Generator {
 
     writing() {
 
+        this.normalizeFilePaths();
+
         const rootGradleSettings = this.destinationPath('android/settings.gradle');
+        const gradleModuleFile = this.destinationPath(`android/${this.answers.module}/build.gradle`);
 
         var tplValues = {
             package: this.answers.package,
@@ -166,7 +174,11 @@ module.exports = class extends Generator {
             targetSdkVersion: this.answers.targetSdkVersion,
             lang: this.answers.lang,
             dependencies: '',
-            remoteGroup: this.answers.remoteGroup
+            remoteGroup: this.answers.remoteGroup,
+            extend: {
+                manifest: '',
+                application: ''    
+            }
         };
 
         if (!this.options.excludeDependencies) {
@@ -174,16 +186,81 @@ module.exports = class extends Generator {
         }
 
         this.fs.copyTpl(
-            this.templatePath(`${this.answers.lang}`),
+            this.templatePath(this.answers.lang),
             this.destinationPath(`android/${this.answers.module}`),
             tplValues
         );
 
         if (this.fs.exists(rootGradleSettings)) {
-            var mainGradle = this.fs.read(rootGradleSettings);
-            mainGradle += `, :'${this.answers.module}'`;
+            var settings = this.fs.read(rootGradleSettings);
+            settings += `, :'${this.answers.module}'`;
 
-            this.fs.write(rootGradleSettings, mainGradle);
+            this.fs.write(rootGradleSettings, settings);
+        }
+
+        if (this.gradleExtraFile) {
+            let gradleModule = this.fs.read(gradleModuleFile);
+            gradleModule = `
+            \rdef hasBuildExtras = file('${path.basename(this.gradleExtraFile.src)}').exists()
+            \rif (hasBuildExtras) {
+                \r\tapply from: '${path.basename(this.gradleExtraFile.src)}'
+            \r}
+            \r` + gradleModule;
+
+            this.fs.write(gradleModuleFile, gradleModule);
+
+            if(this.fs.exists(this.destinationPath(this.gradleExtraFile.src))) {
+                this.fs.copyTpl(
+                    this.destinationPath(this.gradleExtraFile.src),
+                    this.destinationPath(`android/${this.answers.module}/${path.basename(this.gradleExtraFile.src)}`)
+                );
+            }
+        }
+
+        if (this.manifestExtra 
+            && /AndroidManifest/.test(this.manifestExtra.target) 
+            && /application/.test(this.manifestExtra.parent)) {
+
+            this.extendManifestTpl();
+        }
+    }
+
+    end() {
+        
+        if (this.firstSourceFile && /\.java/.test(this.firstSourceFile.src)) {
+            
+            if (this.fs.exists(this.destinationPath(this.firstSourceFile.src))) {
+                
+                this.fs.delete([this.destinationPath('src/android') + '/*.java', '!' + this.destinationPath('src/android') + '/**/*.java']);
+                this.log().info('Cleanup original %s files', '*.java');
+            }
+        }
+    }
+
+    extendManifestTpl() {
+
+        if (this.manifestExtra && this.manifestExtra.parent) {
+            
+            let parentTag = this.manifestExtra.parent.split('/');
+            if (parentTag.length > 0 && parentTag[parentTag.length -1] != '*') {
+                parentTag = parentTag[parentTag.length -1] || 'application'
+            }
+    
+            delete this.manifestExtra.target;
+            delete this.manifestExtra.parent;
+            
+            const manifestXml = xml2json.toXml(this.manifestExtra);
+    
+            this.fs.copyTpl(
+                `${this.sourceRoot().replace('lib', 'app')}/${this.answers.lang}/app/src/main/AndroidManifest.xml`,
+                this.destinationPath('android/app/src/main/AndroidManifest.xml'),
+                {
+                    package: this.answers.package,
+                    extend: {
+                        [parentTag]: xmlBeautifier(manifestXml)    
+                    }
+                }
+            )
         }
     }
 
@@ -192,7 +269,8 @@ module.exports = class extends Generator {
         if (this.fs.exists(this.destinationPath('plugin.xml'))) {
             var xml = this.fs.read(this.destinationPath('plugin.xml'));
             if (xml) {
-                const jsonData = xml2json.toJson(xml, { object: true });
+                
+                const jsonData = xml2json.toJson(xml, { object: true, reversible: true, trim: false });
                 if (jsonData && jsonData.plugin) {
                     const platform = jsonData.plugin.platform.find(p => p.name == 'android');
                     if (platform) {
@@ -202,11 +280,52 @@ module.exports = class extends Generator {
                                 value: 'com.github.mfdeveloper:cordova-android:7.1.1'
                             }
                         ];
-                        this.packagePath = platform['source-file']['target-dir'].replace('src/', '');
+
+                        if (platform['config-file']) {
+                            const cbManifest = c => c.target == 'AndroidManifest.xml';
+                            
+                            if (platform['config-file'] instanceof Array) {
+                                this.manifestExtra = platform['config-file'].find(cbManifest);
+                            } else {
+                                this.manifestExtra = cbManifest(platform['config-file']);
+                            }
+                        }
+
+                        if (platform['framework']) {
+                            this.gradleExtraFile = platform['framework'] instanceof Array ? platform['framework'].find(f => /\.gradle/.test(f.src)) : /\.gradle/.test(platform['framework'].src) ? platform['framework'] : null;
+                        }
+
+                        if (platform['source-file']) {
+                            this.firstSourceFile = platform['source-file'] instanceof Array ? platform['source-file'][0] : platform['source-file'];
+                        }
+                        this.packagePath = this.firstSourceFile['target-dir'].replace('src/', '');
                         this.package = this.packagePath.replace(/\//g, '\.');
                     }
                 }
             }
+        }
+    }
+
+    normalizeFilePaths() {
+        if (this.packagePath) {
+            const fullPackagePath = this.destinationPath(`src/android/${this.packagePath}`);
+            if (!this.fs.exists(fullPackagePath)) {
+                fs.ensureDirSync(fullPackagePath);
+            }
+
+            if (!this.fs.exists(`${fullPackagePath}/${path.basename(this.firstSourceFile.src)}`)) {
+
+                const androidFilesPath = this.destinationPath('src/android');
+
+                try {
+                    this.fs.copy(androidFilesPath + '/*.java', fullPackagePath);
+                } catch (error) {
+                    if (!/does not exist/.test(error.message)) {
+                        throw error;
+                    }
+                }
+            }
+            
         }
     }
 
